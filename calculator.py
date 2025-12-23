ee856f2742c702a3e0ed8da7ff183a5fb3901d56
tinhinane <tintigzirt@gmail.com>
2025-12-23 15:16:18 +0100

    version1

---
+++ calculator.py
@@ -0,0 +1,43 @@
+while True:  
+    print("\nWelcome to the calculator")
+    print("Choose operation:")
+    print("+ Addition")
+    print("- Subtraction")
+    print("* Multiplication")
+    print("/ Division")
+    print("E Exit")
+
+   
+    operation = input("Enter the operation (+, -, *, /, E): ")
+
+    
+    if operation == 'E': 
+        print("Goodbye!")
+        break  
+
+    
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    num3 = float(input("Enter the third number: "))
+
+   
+    if operation == '+':
+        result = num1 + num2 + num3
+    elif operation == '-':
+        result = num1 - num2 - num3
+    elif operation == '*':
+        result = num1 * num2 * num3
+    elif operation == '/':
+        if num2 == 0 and num3 == 0:
+            print("Error: Division by zero!")
+            continue
+        else:
+             result = num1 / num2 / num3
+    else:
+        print("Invalid operation!")
+        continue  
+
+   
+    print("Result:", result)
+
+
